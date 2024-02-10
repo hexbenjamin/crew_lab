233325d1dc2d3d119ce862be513b33511010b64b
hex benjamin <hex@hexbenjam.in>
2024-02-10 16:23:24 -0500

    CREWAI UPDATE DROPPED (also, setting up tool registry)

--- assembler/crew_assembler/tools/__init__.py
+++ assembler/crew_assembler/tools/__init__.py
@@ -1,3 +1,7 @@
 from .youtube_transcript import youtube_transcript_retriever
 
-__all__ = ["youtube_transcript_retriever"]
+registry = {
+    "yt_transcript": youtube_transcript_retriever,
+}
+
+__all__ = ["registry", "youtube_transcript_retriever"]
