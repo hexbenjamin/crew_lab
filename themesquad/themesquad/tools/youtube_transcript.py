e47f107be703c69251a7cdc444311a2eeacd60b6
hex benjamin <hex@hexbenjam.in>
2024-02-10 15:12:19 -0500

    filter for "&..." in youtube links

--- themesquad/themesquad/tools/youtube_transcript.py
+++ themesquad/themesquad/tools/youtube_transcript.py
@@ -19,6 +19,9 @@ def youtube_transcript_retriever(video_url: str) -> str:
     # Extract video id from URL
     video_id = video_url.split("watch?v=")[1]
 
+    if "&" in video_id:
+        video_id = video_id.split("&")[0]
+
     # Get the transcript of the video
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
 
