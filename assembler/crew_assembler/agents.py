88d872603f782929d2014b0cc14b9b2e80e3a239
hex benjamin <hex@hexbenjam.in>
2024-02-11 00:00:16 -0500

    add llm selection!

--- assembler/crew_assembler/agents.py
+++ assembler/crew_assembler/agents.py
@@ -1,37 +1,67 @@
+import os
 from textwrap import dedent
 
-from crew_assembler.tools import Registry
 from crewai import Agent
+from dotenv import load_dotenv
+from langchain.agents import load_tools
 
-# from langchain_openai import ChatOpenAI
+from crew_assembler.tools import Registry
+
+
+load_dotenv(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".env"))
+
+
+def select_llm(provider: str, model: str, base_url: str = None, api_key: str = None):
+    if not api_key:
+        try:
+            api_key = os.environ["OPENAI_API_KEY"] if provider == "openai" else None
+            api_key = (
+                os.environ["HUGGINGFACEHUB_API_TOKEN"] if provider == "hf" else None
+            )
+        except KeyError:
+            pass
+
+    if provider == "ollama":
+        from langchain_community.llms.ollama import Ollama
+
+        llm = Ollama(
+            model=model or "openhermes:7b-mistral-v2.5-q6_K",
+            base_url=base_url or "http://localhost:11434",
+        )
+    elif provider == "openai":
+        from langchain_openai import ChatOpenAI
+
+        llm = ChatOpenAI(
+            model=model or "gpt-3.5-turbo",
+            api_key=api_key or os.environ["OPENAI_API_KEY"] or "sk-...",
+            base_url=base_url or "http://api.openai.com/v1",
+        )
+    elif provider == "hf":
+        from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
+
+        endpoint_url = f"https://api-inference.huggingface.co/models/{model}"
+        llm = HuggingFaceEndpoint(
+            endpoint_url=endpoint_url,
+            huggingfacehub_api_token=api_key or "hf_...",
+            task="text-generation",
+        )
+
+    return llm
 
 
 class AgentBox:
-    def __init__(self, model: str, base_url: str = None, api_key: str = None):
+    def __init__(self, model: str):
         self.toolbox = Registry()
         self.agents = {}
 
-        self.provider, self.model = model.split("/")
+        self.provider, self.model = model.split("/", maxsplit=1)
+
         if not self.provider or not self.model:
             raise ValueError("Model should be in the format <provider>/<model>.")
-        if self.provider not in ["ollama", "openai"]:
-            raise ValueError("Provider should be either 'ollama' or 'openai'.")
+        if self.provider not in ["ollama", "openai", "hf"]:
+            raise ValueError("Provider should be only 'ollama', 'openai', or 'hf'.")
 
-        if self.provider == "ollama":
-            from langchain_community.llms.ollama import Ollama
-
-            self.llm = Ollama(
-                model=self.model or "openhermes:7b-mistral-v2.5-q6_K",
-                base_url=base_url or "http://localhost:11434",
-            )
-        elif self.provider == "openai":
-            from langchain_openai import ChatOpenAI
-
-            self.llm = ChatOpenAI(
-                model=self.model or "gpt-3.5-turbo",
-                api_key=api_key or "sk-000000...",
-                base_url=base_url or "http://api.openai.com/v1",
-            )
+        self.llm = select_llm(provider=self.provider, model=self.model)
 
     def register_agent(self, config: dict):
         tools = (
--- assembler/crew_assembler/tools/__init__.py
+++ assembler/crew_assembler/tools/__init__.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
+
+from langchain.agents import load_tools
+
 from .youtube_transcript import youtube_transcript_retriever
 
 
 @dataclass
 class Registry:
+    human = load_tools(["human"])
     yt_transcript = youtube_transcript_retriever
 
 
