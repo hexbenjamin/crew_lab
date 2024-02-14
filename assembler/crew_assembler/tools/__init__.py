9c82772cdad8501230b38ea82f05460f8115b30d
hex benjamin <hex@hexbenjam.in>
2024-02-14 14:29:27 -0500

    re-enable MemoryTools for testing

--- assembler/crew_assembler/tools/__init__.py
+++ assembler/crew_assembler/tools/__init__.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from langchain.agents import load_tools
 from langchain_community.tools.ddg_search import DuckDuckGoSearchRun
 
-# from .memory import MemoryTools
+from .memory import MemoryTools
 from .web import WebTools
 from .youtube import YouTubeTools
 
@@ -13,8 +13,8 @@ class Registry:
     (_human,) = load_tools(["human"])
     _ddg_search = DuckDuckGoSearchRun()
     html = WebTools.get_html
-    # memory_write = MemoryTools.embed_text
-    # memory_read = MemoryTools.similarity_search
+    memory_write = MemoryTools.embed_text
+    memory_read = MemoryTools.similarity_search
     yt_transcript = YouTubeTools.transcript_retriever
 
 
